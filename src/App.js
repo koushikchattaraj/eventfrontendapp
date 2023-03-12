a8c404196495139ee595d8a2b4a33154af4b0347
koushik <chattarajkoushik@gmail.com>
2023-03-12 22:19:26 +0530

    biyebari

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import { Landing } from "./component/Base/Landing/Landing.js";
 import { useSelector } from "react-redux";
 import { Main } from "./component/Base/Main/Main";
 import { useEffect } from "react";
+import { Event } from "./component/Base/Event/Event";
+import card from "./asset/card.jpg"
 
 function App() {
   const landingPageStatus = useSelector((state) => state.ladndingPageStatus);
 
+  const src={card}
+
   return (
-    <div className="App">{landingPageStatus ? <Landing /> : <Main />}</div>
+    // <div className="App">{landingPageStatus ? <Landing /> : <Main />}</div>
+    <div>
+      <Event src  ={src}/>
+    </div>
+
   );
 }
 
---
+++ src/component/Base/Event/Event.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import card from "../../../asset/card.jpg"
+
+export const Event = (props) => {
+  return (
+    <div>
+        <img src={card} alt="..." className='eventCard' />
+    </div>
+  )
+}
