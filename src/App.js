ef5a5e8a7c06e3ed29203f62f13ce0e8d599efb8
koushik <chattarajkoushik@gmail.com>
2023-03-18 00:03:39 +0530

    chatbot1

--- src/App.js
+++ src/App.js
@@ -53,6 +53,7 @@ const App = () => {
     const configuration = new Configuration({
         apiKey: process.env.REACT_APP_OPENAI_API_KEY,
     });
+    
 
 
     const openai = new OpenAIApi(configuration);
